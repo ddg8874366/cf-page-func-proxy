cd7627f41dc4a356774fe790ee2da04612f9289a
ddg8874366 <104051538+ddg8874366@users.noreply.github.com>
2022-05-22 12:24:10 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
+        url.hostname="cngdg.herokuapp.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
